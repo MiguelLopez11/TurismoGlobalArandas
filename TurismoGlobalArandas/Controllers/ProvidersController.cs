81edeeb329d9fadfa31f4e8993118e64324bf5a9
MiguelLopez11 <miguellopezo1107@gmail.com>
2023-07-07 12:04:37 -0600

    controlers customer and providers

---
+++ TurismoGlobalArandas/Controllers/ProvidersController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TurismoGlobalArandas.Context;
+using TurismoGlobalArandas.Models;
+
+namespace TurismoGlobalArandas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvidersController : ControllerBase
+    {
+        private readonly TurismoGlobalContext _context;
+        public ProvidersController(TurismoGlobalContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Providers>> getProviders()
+        {
+            var providers = await _context.Providers
+                .Where(w => !w.isDeleted)
+                .ToListAsync();
+            return Ok(providers);
+        }
+        [HttpGet("{CustomerId}")]
+        public async Task<ActionResult> getProviderById(int ProviderId)
+        {
+            var provider = await _context.Providers
+                .Where(w => !w.isDeleted)
+                .FirstOrDefaultAsync(f => f.ProviderId == ProviderId);
+            if (provider == null)
+            {
+                return NotFound();
+            }
+            return Ok(provider);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Providers>> PostProvider(Providers Provider)
+        {
+            _context.Providers.Add(Provider);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("getProviderById", new { ProviderId = Provider.ProviderId }, Provider);
+        }
+        [HttpPut("{ProviderId}")]
+        public async Task<ActionResult> PutProvider(int ProviderId, Providers Provider)
+        {
+            if (Provider.ProviderId != ProviderId)
+            {
+                return Ok("Los Id ingresados no coinciden");
+            }
+            var ProviderOld = await _context.Providers.FirstOrDefaultAsync(f => f.ProviderId == ProviderId);
+            if (ProviderOld == null)
+            {
+                return BadRequest($"El cliente con el ID {ProviderId} no existe");
+            }
+            ProviderOld.ProviderId = Provider.ProviderId;
+            ProviderOld.Name = Provider.Name;
+            ProviderOld.Email = Provider.Email;
+            ProviderOld.PhoneNumber = Provider.PhoneNumber;
+            ProviderOld.isDeleted = Provider.isDeleted;
+
+            _context.Providers.Update(ProviderOld);
+            await _context.SaveChangesAsync();
+            return Ok("El cliente se actualizo correctamente");
+        }
+        [HttpDelete("{ProviderId}")]
+        public async Task<IActionResult> DeleteProvider(int ProviderId)
+        {
+            var provider = await _context.Providers
+                .FirstOrDefaultAsync(f => f.ProviderId == ProviderId);
+            if (provider == null)
+            {
+                return NotFound();
+            }
+
+            provider.isDeleted = true;
+            _context.Providers.Update(provider);
+            await _context.SaveChangesAsync();
+            return Ok("Proveedor archivado");
+        }
+    }
+}
