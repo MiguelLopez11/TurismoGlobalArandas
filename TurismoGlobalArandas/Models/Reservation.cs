93bf451bae61907a73e6028a1083877bc304c9ab
MiguelLopez11 <miguellopezo1107@gmail.com>
2023-07-17 19:33:33 -0600

    reservation model

---
+++ TurismoGlobalArandas/Models/Reservation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TurismoGlobalArandas.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int ReservationId { get; set; }
+        [Required]
+        public string reservationInvoice {get;set;}
+        public int CustomerId { get;set; }
+        [ForeignKey("CustomerId")]
+        public Customers? Customers { get; set; }
+        public string Hotel { get; set; }
+        public string Destination { get; set; }
+        public DateTime TravelDate { get; set; }
+        public string Observation { get; set; }
+    }
+}
