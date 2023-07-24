adfa4e4c8f872a8d9b58b5fcf194852e95413925
MiguelLopez11 <miguellopezo1107@gmail.com>
2023-07-24 17:05:31 -0600

    hotels controller

---
+++ TurismoGlobalArandas/Migrations/20230724225423_hotels.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TurismoGlobalArandas.Migrations
+{
+    /// <inheritdoc />
+    public partial class hotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Ages",
+                table: "Habitations",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hotels_DestinationId",
+                table: "Hotels",
+                column: "DestinationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Destinations_DestinationId",
+                table: "Hotels",
+                column: "DestinationId",
+                principalTable: "Destinations",
+                principalColumn: "DestinationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Destinations_DestinationId",
+                table: "Hotels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Hotels_DestinationId",
+                table: "Hotels");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Ages",
+                table: "Habitations",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
