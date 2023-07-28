064ee25b888a76b74e86173b8ff908c1dd296b1a
MiguelLopez11 <miguellopezo1107@gmail.com>
2023-07-27 18:17:55 -0600

    reservaciones y tarifa

--- TurismoGlobalArandas/Models/Reservations.cs
+++ TurismoGlobalArandas/Models/Reservations.cs
@@ -8,16 +8,21 @@ namespace TurismoGlobalArandas.Models
         [Key]
         public int ReservationId { get; set; }
         [Required]
-        public string reservationInvoice {get;set;}
+        public string TypeReservationId {get;set;}
+        [Required]
+        public string ReservationInvoice {get;set;}
         public int CustomerId { get;set; }
         public int HotelId { get; set; }
         public DateTime TravelDate { get; set; }
-        public string Observation { get; set; }
+        public string TypeHabitation { get; set; }
+        public string Observations { get; set; }
         public bool IsDeleted { get; set; }
 
         [ForeignKey("CustomerId")]
         public Customers? Customers { get; set; }
         [ForeignKey("HotelId")]
         public Hotels? Hotels { get; set; }
+        [ForeignKey("TypeReservationId")]
+        public TypeReservation? TypeReservation { get; set; }
     }
 }
---
+++ TurismoGlobalArandas/Models/SingleRate.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TurismoGlobalArandas.Models
+{
+    public class SingleRate
+    {
+        [Key] public int SingleRateId { get; set; }
+        public decimal PublicRate { get; set; }
+        public double ExtraDiscount { get; set; }
+        public decimal RateDiscounted { get; set; }
+        public decimal RateClient { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
