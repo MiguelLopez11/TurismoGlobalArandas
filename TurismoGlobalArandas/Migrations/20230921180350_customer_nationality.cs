1e6459bd51f2db3874c6758efcdc58d258267dc3
MiguelLopez11 <miguellopezo1107@gmail.com>
2023-09-21 12:10:23 -0600

    customer nationality

---
+++ TurismoGlobalArandas/Migrations/20230921180350_customer_nationality.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TurismoGlobalArandas.Migrations
+{
+    /// <inheritdoc />
+    public partial class customer_nationality : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IndividualRates_ReservationHotels_ReservationHotelId",
+                table: "IndividualRates");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReservationHotelId",
+                table: "IndividualRates",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PublicRate",
+                table: "IndividualRates",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExtraDiscount",
+                table: "IndividualRates",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ClientRate",
+                table: "IndividualRates",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nationality",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IndividualRates_ReservationHotels_ReservationHotelId",
+                table: "IndividualRates",
+                column: "ReservationHotelId",
+                principalTable: "ReservationHotels",
+                principalColumn: "ReservationHotelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IndividualRates_ReservationHotels_ReservationHotelId",
+                table: "IndividualRates");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReservationHotelId",
+                table: "IndividualRates",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PublicRate",
+                table: "IndividualRates",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExtraDiscount",
+                table: "IndividualRates",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ClientRate",
+                table: "IndividualRates",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nationality",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IndividualRates_ReservationHotels_ReservationHotelId",
+                table: "IndividualRates",
+                column: "ReservationHotelId",
+                principalTable: "ReservationHotels",
+                principalColumn: "ReservationHotelId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
