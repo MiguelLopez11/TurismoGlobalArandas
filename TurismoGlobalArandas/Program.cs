452a6f77c621de37d34d155d7c562b39c4de2f89
MiguelLopez11 <miguellopezo1107@gmail.com>
2023-12-07 00:36:02 -0600

    Deploy

--- TurismoGlobalArandas/Program.cs
+++ TurismoGlobalArandas/Program.cs
@@ -1,48 +1,46 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
 using System.Text;
 using TurismoGlobalArandas.Context;
-using Microsoft.OpenApi.Models;
-using UConnect.Entities;
-using Microsoft.Extensions.FileProviders;
 using TurismoGlobalArandas.Models;
-using TurismoGlobalArandas.Services;
+//using TurismoGlobalArandas.Services;
+using UConnect.Entities;
 
 namespace TurismoGlobalArandas
 {
     public class Program
     {
-        private IConfiguration _configuration;
-
         public static void Main(string[] args)
         {
+            // Crear el constructor de la aplicación
             var builder = WebApplication.CreateBuilder(args);
             var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-            ConfigurationManager configuration = builder.Configuration;
-            //CONECTION DATABASE
+
+            // Configuración de la aplicación
+            var configuration = builder.Configuration;
+
+            // Configuración de la base de datos
             builder.Services.AddDbContext<TurismoGlobalContext>(options =>
             {
-                options.UseSqlServer(
-                    builder.Configuration.GetConnectionString("DefaultConnection")
-                );
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             });
-            //IDENTITY CONFIGURATION
+
+            // Configuración de Identity
             builder.Services
                 .AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<TurismoGlobalContext>()
                 .AddDefaultTokenProviders();
 
-            // Adding Authentication
-            builder.Services
-                .AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                // Adding Jwt Bearer
+            // Configuración de la autenticación JWT
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
                     options.SaveToken = true;
@@ -56,103 +54,87 @@ namespace TurismoGlobalArandas
                         ClockSkew = TimeSpan.MaxValue,
                         ValidAudience = configuration["JWT:ValidAudience"],
                         ValidIssuer = configuration["JWT:ValidIssuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(
-                            Encoding.UTF8.GetBytes(configuration["JWT:Secret"])
-                        )
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
                     };
                 });
 
+            // Configuración de servicios
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddHttpContextAccessor();
-            builder.Services.AddHostedService<NotificacionService>();
-            builder.Services.AddScoped<TurismoGlobalContext>();
-            //CORS
+            //builder.Services.AddHostedService<NotificacionService>();
+            //builder.Services.AddScoped<TurismoGlobalContext>();
+
+            // Configuración de CORS
             builder.Services.AddCors(options =>
             {
-                options.AddPolicy(
-                    name: MyAllowSpecificOrigins,
-                    policy =>
-                    {
-                        policy.WithOrigins("http://localhost:8080");
-                        policy.WithOrigins("https://turismoglobalarandas.onrender.com");
-                        policy.AllowAnyHeader();
-                        policy.AllowCredentials();
-                        policy.AllowAnyMethod();
-                    }
-                );
+                options.AddPolicy(MyAllowSpecificOrigins, policy =>
+                {
+                    policy.WithOrigins("http://localhost:8080", "https://turismoglobalarandas.onrender.com")
+                          .AllowAnyHeader()
+                          .AllowCredentials()
+                          .AllowAnyMethod();
+                });
             });
-            //SWAGGER User configuration
+
+            // Configuración de Swagger
             builder.Services.AddSwaggerGen(option =>
             {
-                option.SwaggerDoc(
-                    "v1",
-                    new OpenApiInfo { Title = "TurismoGlobalArandas_WebServices", Version = "v1" }
-                );
-                option.AddSecurityDefinition(
-                    "Bearer",
-                    new OpenApiSecurityScheme
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "TurismoGlobalArandas_WebServices", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
                     {
-                        In = ParameterLocation.Header,
-                        Description = "Please enter a valid token",
-                        Name = "Authorization",
-                        Type = SecuritySchemeType.Http,
-                        BearerFormat = "JWT",
-                        Scheme = "Bearer"
-                    }
-                );
-                option.AddSecurityRequirement(
-                    new OpenApiSecurityRequirement
-                    {
+                        new OpenApiSecurityScheme
                         {
-                            new OpenApiSecurityScheme
+                            Reference = new OpenApiReference
                             {
-                                Reference = new OpenApiReference
-                                {
-                                    Type = ReferenceType.SecurityScheme,
-                                    Id = "Bearer"
-                                }
-                            },
-                            new string[] { }
-                        }
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
                     }
-                );
+                });
             });
-            
 
+            // Construir la aplicación
             var app = builder.Build();
-            var env = app.Environment;
-            //SERVICE FILES UPLOAD
+
+            // Configuración de archivos estáticos
             if (app.Environment != null)
             {
                 app.UseStaticFiles();
-                app.UseStaticFiles(
-                    new StaticFileOptions
-                    {
-                        FileProvider = new PhysicalFileProvider(
-                            Path.Combine(app.Environment.ContentRootPath, "Resource")
-                        ),
-                        RequestPath = "/Resource/Files"
-                    }
-                );
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "Resource")),
+                    RequestPath = "/Resource/Files"
+                });
             }
 
+            // Configuración para desarrollo
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            // Configuración general
             app.UseSwagger();
             app.UseSwaggerUI();
             app.UseCors(MyAllowSpecificOrigins);
             app.UseHttpsRedirection();
-
             app.UseAuthentication();
             app.UseAuthorization();
-
             app.MapControllers();
-
             app.Run();
         }
     }
