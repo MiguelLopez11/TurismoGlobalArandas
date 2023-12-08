a8eb6f283ac103a4f59ead4b4d01d88ca876801c
MiguelLopez11 <miguellopezo1107@gmail.com>
2023-12-08 14:47:44 -0600

    correcciones

--- TurismoGlobalArandas/Services/NotificacionService.cs
+++ TurismoGlobalArandas/Services/NotificacionService.cs
@@ -1,84 +1,84 @@
-﻿//using Microsoft.AspNetCore.Http.HttpResults;
-//using Microsoft.EntityFrameworkCore;
-//using TurismoGlobalArandas.Context;
-//using TurismoGlobalArandas.Models;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using TurismoGlobalArandas.Context;
+using TurismoGlobalArandas.Models;
 
-//namespace TurismoGlobalArandas.Services
-//{
-//    public class NotificacionService : IHostedService, IDisposable
-//    {
-//        private readonly IServiceProvider _serviceProvider;
-//        private Timer? _timer = null;
-//        public NotificacionService(IServiceProvider serviceProvider)
-//        {
-//            _serviceProvider = serviceProvider;
-//        }
-//        public Task StartAsync(CancellationToken stoppingToken)
-//        {
+namespace TurismoGlobalArandas.Services
+{
+    public class NotificacionService : IHostedService, IDisposable
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private Timer? _timer = null;
+        public NotificacionService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
 
-//            _timer = new Timer(async _ => await DoWorkAsync(), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            _timer = new Timer(async _ => await DoWorkAsync(), null, TimeSpan.Zero, TimeSpan.FromDays(1));
 
-//            return Task.CompletedTask;
-//        }
-//        private async Task DoWorkAsync()
-//        {
-//            try
-//            {
-//                // Lógica para obtener reservaciones cercanas a la fecha de pago
-//                var scope = _serviceProvider.CreateScope();
-               
-//                    var context = scope.ServiceProvider.GetRequiredService<TurismoGlobalContext>();
-//                    var Reservations = await ObtenerReservacionesCercanas();
+            return Task.CompletedTask;
+        }
+        private async Task DoWorkAsync()
+        {
+            try
+            {
+                // Lógica para obtener reservaciones cercanas a la fecha de pago
+                var scope = _serviceProvider.CreateScope();
 
-//                    // Generar notificaciones
-//                    foreach (var reservacion in Reservations)
-//                    {
-//                        TimeSpan? DifferenceDays = reservacion.PaymentLimitDate - DateTime.UtcNow;
-//                        int DaysFailing = (int)DifferenceDays?.TotalDays;
+                var context = scope.ServiceProvider.GetRequiredService<TurismoGlobalContext>();
+                var Reservations = await ObtenerReservacionesCercanas();
 
-//                        var notificacion = new Notifications
-//                        {
-//                            Title = "Próxima fecha de pago: " + reservacion.PaymentLimitDate,
-//                            Message = $"Te recordamos que el pago de la reservación con el folio: {reservacion.Invoice} está próximo a vencer.",
-//                            Created = DateTime.UtcNow,
-//                            IsReaded = false
-//                        };
+                // Generar notificaciones
+                foreach (var reservacion in Reservations)
+                {
+                    TimeSpan? DifferenceDays = reservacion.PaymentLimitDate - DateTime.UtcNow;
+                    int DaysFailing = (int)DifferenceDays?.TotalDays;
 
-//                        context.Notifications.Add(notificacion);
-//                        await context.SaveChangesAsync();
-//                    }
-                
-//            }
-//            catch (Exception ex)
-//            {
-//                Console.WriteLine(ex.ToString());
-//            }
-//        }
-//        public Task StopAsync(CancellationToken stoppingToken)
-//        {
-//            _timer?.Change(Timeout.Infinite, 0);
+                    var notificacion = new Notifications
+                    {
+                        Title = "Próxima fecha de pago: " + reservacion.PaymentLimitDate,
+                        Message = $"Te recordamos que el pago de la reservación con el folio: {reservacion.Invoice} está próximo a vencer.",
+                        Created = DateTime.UtcNow,
+                        IsReaded = false
+                    };
 
-//            return Task.CompletedTask;
-//        }
+                    context.Notifications.Add(notificacion);
+                    await context.SaveChangesAsync();
+                }
 
-//        public void Dispose()
-//        {
-//            _timer?.Dispose();
-//            GC.SuppressFinalize(this);
-//        }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
 
-//        private async Task<List<ReservationHotel>> ObtenerReservacionesCercanas()
-//        {
-//            var scope = _serviceProvider.CreateScope();
-            
-//                var context = scope.ServiceProvider.GetRequiredService<TurismoGlobalContext>();
-//                var fechaLimite = DateTime.UtcNow.AddDays(3);
-//                return await context.ReservationHotels
-//                    .Where(r => r.PaymentLimitDate.HasValue && r.PaymentLimitDate <= fechaLimite)
-//                    .ToListAsync();
-            
+            return Task.CompletedTask;
+        }
 
-//        }
-//    }
+        public void Dispose()
+        {
+            _timer?.Dispose();
+            GC.SuppressFinalize(this);
+        }
 
-//}
+        private async Task<List<ReservationHotel>> ObtenerReservacionesCercanas()
+        {
+            var scope = _serviceProvider.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<TurismoGlobalContext>();
+            var fechaLimite = DateTime.UtcNow.AddDays(3);
+            return await context.ReservationHotels
+                .Where(r => r.PaymentLimitDate.HasValue && r.PaymentLimitDate <= fechaLimite)
+                .ToListAsync();
+
+
+        }
+    }
+
+}
